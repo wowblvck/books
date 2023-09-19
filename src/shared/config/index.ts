545c0fb16c88c6507cfdc26625c99625d3251ec9
Indar Basto <indar.basto@gmail.com>
2023-09-19 13:22:55 +0300

    feat: Netlify CI/CD pipeline

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,16 @@
+import { z } from 'zod';
+
+const envVariables = z.object({
+  VITE_API_ENDPOINT: z.string().url(),
+  VITE_API_KEY: z.string(),
+});
+
+envVariables.parse(import.meta.env);
+
+declare global {
+  interface ImportMetaEnv extends z.infer<typeof envVariables> {}
+}
+
 export const config = {
   API_ENDPOINT: import.meta.env.VITE_API_ENDPOINT,
   API_KEY: import.meta.env.VITE_API_KEY,
